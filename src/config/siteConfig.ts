0ac70cab58f8d8394d4118ae04d1ef125d9cf384
Wang Mingzhen <491453898@qq.com>
2026-07-22 15:19:38 +0800

    style: 将站点标题、横幅主标题与个人名片名称修改为 luck007

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,15 +12,15 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Luck007",
 
 	// 站点 URL
-	site_url: "https://luck007.cuteleaf.cn",
+	site_url: "https://firefly.cuteleaf.cn",
 
 	// 站点描述
 	description:
-		"Luck007 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
 
 	// 站点关键词
 	keywords: [
-		"Luck007",
+		"Firefly",
 		"Fuwari",
 		"Astro",
 		"ACGN",
@@ -78,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "luck007",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
