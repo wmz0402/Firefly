163646bde9fc8c0dab0302f316a5e3e1ee035e59
Wang Mingzhen <491453898@qq.com>
2026-07-24 10:11:41 +0800

    feat: add new dynamic item

--- src/config/dynamicConfig.ts
+++ src/config/dynamicConfig.ts
@@ -20,5 +20,11 @@ export const dynamicConfig: DynamicConfig = {
 	title: "最新动态",
 	moreText: "更多动态",
 	moreUrl: "/dynamic",
-	items: [],
+	items: [
+		{
+			date: "2026/07/24 10:11",
+			content: "今天好开心因为明天就是周末了",
+			pinned: false,
+		},
+	],
 };
