88a339e35d8e60bbd272b79782f03452d67d6d33
Wang Mingzhen <491453898@qq.com>
2026-07-24 09:21:37 +0800

    update: clear dynamics/old gallery and add new album 运动会2026

--- src/config/dynamicConfig.ts
+++ src/config/dynamicConfig.ts
@@ -20,16 +20,5 @@ export const dynamicConfig: DynamicConfig = {
 	title: "最新动态",
 	moreText: "更多动态",
 	moreUrl: "/dynamic",
-	items: [
-		{
-			date: "2026/07/23 15:04",
-			content: "这是一条memos的动态",
-			pinned: true,
-		},
-		{
-			date: "2026/07/23 15:42",
-			content: "飞萤之火自无梦的长夜亮起，绽放在终竞的明天。",
-			pinned: false,
-		},
-	],
+	items: [],
 };
