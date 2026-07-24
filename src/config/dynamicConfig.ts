96fdf9772b26422b2d9542af9e3208f4a1f34679
Wang Mingzhen <491453898@qq.com>
2026-07-24 12:23:26 +0800

    feat: add birthday dynamic for Li Tingting

--- src/config/dynamicConfig.ts
+++ src/config/dynamicConfig.ts
@@ -22,6 +22,11 @@ export const dynamicConfig: DynamicConfig = {
 	moreUrl: "/dynamic",
 	items: [
 		{
+			date: "2026/07/24 12:22",
+			content: "今天是李婷婷的生日，祝李婷婷十九岁生日快乐",
+			pinned: false,
+		},
+		{
 			date: "2026/07/24 10:11",
 			content: "今天好开心因为明天就是周末了",
 			pinned: false,
